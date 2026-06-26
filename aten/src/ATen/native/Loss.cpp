81d936d8eef20f21d3c8ff2059b9b0a3ccb57fd7
jyz <jyz@DUT7357PVC.jf.intel.com>
2026-06-26 01:13:31 +0000

    Fix #1857: BCEWithLogitsLoss target validation

--- aten/src/ATen/native/Loss.cpp
+++ aten/src/ATen/native/Loss.cpp
@@ -15,8 +15,10 @@
 #include <ATen/Functions.h>
 #include <ATen/NativeFunctions.h>
 #else
+#include <ATen/ops/_assert_async.h>
 #include <ATen/ops/binary_cross_entropy_backward_native.h>
 #include <ATen/ops/binary_cross_entropy_native.h>
+#include <ATen/ops/_assert_async.h>
 #include <ATen/ops/binary_cross_entropy_with_logits_native.h>
 #include <ATen/ops/clamp_min.h>
 #include <ATen/ops/cosine_embedding_loss_native.h>
@@ -347,6 +349,15 @@ Tensor& binary_cross_entropy_backward_out_cpu(const Tensor& grad, const Tensor& 
 }
 
 Tensor binary_cross_entropy_with_logits(const Tensor& input, const Tensor& target, const std::optional<Tensor>& weight_opt, const std::optional<Tensor>& pos_weight_opt, int64_t reduction) {
+<<<<<<< Updated upstream
+  at::_assert_async(
+      (target >= 0).logical_and_(target <= 1).all(),
+      "all elements of target should be between 0 and 1");
+
+=======
+  at::_assert_async((target >= 0.) & (target <= 1.),
+      "all elements of target should be between 0 and 1");
+>>>>>>> Stashed changes
   auto log_sigmoid_input = at::log_sigmoid(input);
   if (pos_weight_opt.has_value() && pos_weight_opt->defined()) {
       // pos_weight need to be broadcasted, thus mul(target) is not inplace.
